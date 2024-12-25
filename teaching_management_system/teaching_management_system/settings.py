2356720e4fb2279749706fedd8038d04ee23c678
Roman <roman@tiangong.edu.cn>
2024-12-25 17:25:09 +0800

    no corees

--- teaching_management_system/teaching_management_system/settings.py
+++ teaching_management_system/teaching_management_system/settings.py
@@ -42,6 +42,7 @@ INSTALLED_APPS = [
     'teachers',
     'courses',
     'administration',
+    'corsheaders',
 ]
 
 MIDDLEWARE = [
@@ -52,7 +53,11 @@ MIDDLEWARE = [
     'django.contrib.auth.middleware.AuthenticationMiddleware',
     'django.contrib.messages.middleware.MessageMiddleware',
     'django.middleware.clickjacking.XFrameOptionsMiddleware',
+    'corsheaders.middleware.CorsMiddleware',
+    'django.middleware.common.CommonMiddleware',
 ]
+
+CORS_ALLOW_ALL_ORIGINS = True
 
 ROOT_URLCONF = 'teaching_management_system.urls'
 
